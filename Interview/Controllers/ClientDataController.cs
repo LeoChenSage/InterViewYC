50e808ff55acc69ad6a42bb71f0ea8d8834b5e07
mashung@gmail.com <mashung@gmail.com>
2017-05-22 23:28:42 +0800

    讀跟寫 沒有套版

---
+++ Interview/Controllers/ClientDataController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using InterViewModel;
+namespace Interview.Controllers
+{
+    public class ClientDataController : Controller
+    {
+        // GET: ClientData
+        public ActionResult Index()
+        {
+            var item = ClientData.GetClients();
+            return View(item);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            var citys = KeyValues.GetKeyValues();
+            ViewBag.Citys = citys;
+            if (id.HasValue)
+            {
+                var item = ClientData.GetClientById(id.Value);
+                ViewBag.EditMode = "編輯";
+                return View(item);
+            }
+            ViewBag.EditMode = "新增";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Interview.Models.Container.ClientData clientData)
+        {
+            string result = string.Empty;
+            try
+            {
+                if (ClientData.Insert(clientData))
+                    result = "成功";
+                else
+                    result = "失敗";
+                TempData["Result"] = result;
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- Interview/Models/ClientData.cs
+++ Interview/Models/ClientData.cs
@@ -8,15 +8,49 @@ namespace InterViewModel
 {
     public partial class ClientData
     {
+        public static List<Interview.Models.Container.ClientData> GetClients()
+        {
+            try
+            {
+                var item = new List<Interview.Models.Container.ClientData>();
+                using (var context = new HouseRulesEntities())
+                {
+                    item = context.ClientData.Where(x => x.Status == 1).ToList();
+                }
+                return item;
+            }
+            catch
+            {
+                return new List<Interview.Models.Container.ClientData>();
+            }
+        }
+
+        public static Interview.Models.Container.ClientData GetClientById(int id)
+        {
+            try
+            {
+                var item = new Interview.Models.Container.ClientData();
+                using (var context = new HouseRulesEntities())
+                {
+                    item = context.ClientData.FirstOrDefault(x => x.Status == 1 && x.ID == id);
+                }
+                return item;
+            }
+            catch
+            {
+                return new Interview.Models.Container.ClientData();
+            }
+        }
+
         public static bool Update(Interview.Models.Container.ClientData clientData)
         {
             try
             {
-                using(var context = new HouseRulesEntities())
+                using (var context = new HouseRulesEntities())
                 {
-                    var item = context.ClientData.FirstOrDefault(x=>x.ID == clientData.ID);
+                    var item = context.ClientData.FirstOrDefault(x => x.ID == clientData.ID);
 
-                    if(item == null)
+                    if (item == null)
                         return false;
                     context.Entry(item).CurrentValues.SetValues(item);
                 }
@@ -34,17 +68,19 @@ namespace InterViewModel
             {
                 using (var context = new HouseRulesEntities())
                 {
+                    clientData.CreateDate = DateTime.Now;
+                    clientData.UpdateDate = DateTime.Now;
+                    clientData.Status = 1;
                     context.ClientData.Add(clientData);
                     context.SaveChanges();
                 }
                 return true;
             }
-            catch
+            catch(Exception ex)
             {
                 return false;
             }
         }
 
-        public static bool
     }
 }
